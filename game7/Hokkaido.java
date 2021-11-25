82da0b1f2e3bef478fd2292bdc39ecb39a1654d4
afiqfadzil <afiq.fadzil.99@gmail.com>
2021-11-25 11:56:32 +0900

    afiq outer wall

---
+++ game7/Hokkaido.java
@@ -0,0 +1,65 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hokkaido here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hokkaido extends World
+{
+
+    /**
+     * Constructor for objects of class Hokkaido.
+     * 
+     */
+    public Hokkaido()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        
+        super(800, 815, 1); 
+        addObject(new TARO(),55,50); 
+        addObject(new squid(),700,750); 
+        for( int x = 0 ; x<=815 ;x++){
+        addObject(new wall(),x,0); 
+        }
+         for( int x = 0 ; x<=815 ;x++){
+        addObject(new wall(),x,815); 
+        }
+         for( int y = 0 ; y<=800 ;y++){
+        addObject(new wall(),0,y); 
+        }
+         for( int y = 0 ; y<=800 ;y++){
+        addObject(new wall(),815,y); 
+        }
+  
+        //maze wall part
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         
+        for(int i = 0 ; i < 5  ; i++){  
+
+        int A = 10; 
+
+        int B = 750; 
+
+        int x = A + (int)(Math.random()*((B-A)+1)); 
+
+        B= 450; 
+
+        int y = B - (int)(Math.random()*((B-A)+1)); 
+
+        addObject(new esa1(),x,y); 
+        
+    }
+    }
+    }
+
--- game7/wall.java
+++ game7/wall.java
@@ -1,24 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class start_button here.
+ * Write a description of class wall here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class start_button extends Actor
+public class wall extends Actor
 {
     /**
-     * Act - do whatever the start_button wants to do. This method is called whenever
+     * Act - do whatever the wall wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-      if (Greenfoot.mouseClicked(this))
- 
-{
-Greenfoot.setWorld(new Hokkaido());
-}
-        
+        // Add your action code here.
     }    
 }
